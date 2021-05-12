bb764ccd39e64056f7c9e0e63c6a6ce819b1cf6a
XtreMeIGravity <xtremeigraviti@hotmail.com>
2021-05-12 02:15:51 -0500

    xd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PiComponent } from './pi/pi.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PiComponent
   ],
   imports: [
     BrowserModule,
